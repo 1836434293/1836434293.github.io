790ac0d331dc499ebc159cbc2bb71f57a45f39d6
MengZhe <1836434293@qq.com>
2020-07-13 19:57:55 +0800

    每时每课

---
+++ Each-lesson/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+   
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import xinfeng from './utils/xinfeng'
+export default {
+  
+}
+</script>
+
+<style lang="less">
+*{
+  margin: 0;
+  padding: 0;
+  font-weight: normal;
+}
+body{
+  background: #f0f2f5;
+}
+</style>
